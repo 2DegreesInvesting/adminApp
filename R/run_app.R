820ead7e5d603b758522a5c80717c5c3ea887cd5
Mauro Lepore <maurolepore@gmail.com>
2021-07-05 20:58:15 -0600

    Style

--- R/run_app.R
+++ R/run_app.R
@@ -11,8 +11,7 @@ run_app <- function() {
   ui <- fluidPage(tabsetPanel(id = "tabs", tabPanelCalculate(), tabPanelInfo()))
 
   server <- function(input, output, session) {
-    n_days <-
-      reactive(count_workdays(input$range[[1]], input$range[[2]]))
+    n_days <- reactive(count_workdays(input$range[[1]], input$range[[2]]))
     output$count <- renderText(n_days())
     output$charge <- renderText(n_days() * input$daily_rate)
   }
